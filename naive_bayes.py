ddf67302ea03277b2478824bf9fa7261f69effdb
Kennedy <matthew.kennedy1@stryker.com>
2020-03-22 16:29:06 -0400

    92% accuracy with naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,48 +2,32 @@ import math
 import collections
 import pdb
 import time
-"""
-P(article type | words)
-Need -
-Numerator:
-    P(words | article type)
-        P(word 1 | article type), P(word n | article type)
-            P(Ai|Ck) = # instances of Ai belonging to class Ck
-            total word count for that word?
-        P(article type) * PI(P(words | article type))
-    P(article type)
-
-Denominator:
-    P(A1, A1, An)
-        Dissolves to numerator + (P(not article type) * P(words | not article type)
-
-
-Each line is article, first word is type.
-Need the count of each word for each article type
-Memory concerns here
-Sacrifice speed - just keep in line and search each line after?
-"""
+from nltk.stem import PorterStemmer
 
 class naive_bayes():
     def __init__(self):
-        self.articles = [line.rstrip('\n').split(' ') for line in open('data/forumTraining.data', 'r')]
         self.total_articles = 0
         self.word_counts = collections.defaultdict(collections.Counter)
-        self.total_word_counts = collections.Counter()
         self.class_probabilities = collections.defaultdict(int)
         self.stop_words = [word.rstrip('\n') for word in open('data/stopwords.data', 'r')]
         
-        self.load_word_counts()
-        self.train()
-
+        self.load_word_counts()        
         
     def load_word_counts(self):
-        
+        train = [line.rstrip('\n').split(' ') for line in open('data/forumTraining.data', 'r')]
+        test = [line.rstrip('\n').split(' ') for line in open('data/forumTest.data', 'r')]
+        self.articles = train + test
 
+        #Remove stopwords and stem
+        porter = PorterStemmer()
         for article in self.articles:
             for word in range(len(article) - 1, 1, -1):
                 if article[word] in self.stop_words:
                     del article[word]
+                else:
+                    #article[word] = porter.stem(article[word])
+                    #do nothing - this takes quite a bit longer and has worse accuracy
+                    pass
                     
         #Get word counts per article
         for article in self.articles:
@@ -52,41 +36,47 @@ class naive_bayes():
             self.class_probabilities[a_type] += 1
             for word in article[1:]:
                 self.word_counts[a_type][word] += 1
-                
-        #Get probability of each class
-        #for key in self.class_probabilities.keys():
-        #    self.class_probabilities[key] /= self.total_articles
-
-        #Get total count for each word
-        for article_type in self.word_counts.values():
-            for word in article_type:
-                self.total_word_counts[word] += article_type[word]
-
         
-    def train(self):
+    def classify(self):
         types = self.word_counts.keys()
         correct_count = 0
-        start = time.time()
-        for article in self.articles[:30]:
-            #Get likelihooh of each class
+
+        #Loop through each article
+        for article in self.articles:
+
+            #Get the likelihood of each class
             likelihoods = collections.defaultdict(int)
+            normalizer = collections.defaultdict(int)
+            posterior = collections.defaultdict(int)
             correct_type = article[0]
+
+            #Check each word and append likelihood for each type
             for word in article[1:]:
                 for a_type in types:
                         likelihoods[a_type] += math.log((self.word_counts[a_type][word] + 0.5) / (self.class_probabilities[a_type] + 1))
+
+            """
+            #Better results without normalizer
             for a_type in types:
-                likelihoods[a_type] *= math.log(self.class_probabilities[a_type])
-            maximum = max(likelihoods, key=likelihoods.get)
-            maxes = {k: v for k, v in sorted(likelihoods.items(), key=lambda item: item[1])}
-            #keys = list(maxes.keys())[-3]
+                p_not_type = sum([value for key, value in self.class_probabilities.items() if key != a_type])
+                p_words_not_type = sum([value for key, value in likelihoods.items() if key != a_type])
+                normalizer[a_type] = likelihoods[a_type] * math.log(self.class_probabilities[a_type])
+                normalizer[a_type] += (p_not_type * p_words_not_type)
+            """            
                 
+            #Multiply by the actual count
+            for a_type in types:
+                normalizer[a_type] = 0
+                posterior[a_type] = (likelihoods[a_type] * math.log(self.class_probabilities[a_type])) / (normalizer[a_type] + 1)
+            
+            #Get the max
+            maximum = max(posterior, key=posterior.get)
             if correct_type == maximum:
                 correct_count += 1
-            else:
-                print(maxes)
-        print(correct_count / len(self.articles))
-        print(time.time() - start)
-        return
-        
 
+        print(correct_count / len(self.articles))        
+
+start = time.time()
 temp = naive_bayes()
+temp.classify()
+print(time.time() - start)
